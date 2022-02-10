6a7051fb74d9eead682ec4c2dbc945d673bbbb86
gellwanger <gustavojce@gmail.com>
2022-02-09 22:36:36 -0300

    Adiciona funçoes de deposito, saque e transfere

--- Caixa Eletrônico/conta.py
+++ Caixa Eletrônico/conta.py
@@ -1,7 +1,24 @@
+from pyrsistent import v
+
+
 class Conta:
     def __init__(self, numero, titular, saldo, limite):
         print('Imprimindo objeto...{}'.format(self))
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
+
+    def extrato(self):
+        print('Saldo R${} do titular {}'.format(self.__saldo, self.__titular))
+
+    def deposito(self, valor):
+        self.__saldo += valor
+
+    def saque(self, valor):
+        self.__saldo -= valor
+
+    def transfere(self, valor, origem, destino):
+        origem.saque(valor)
+        destino.deposito(valor)
+        
