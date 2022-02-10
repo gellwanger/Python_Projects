f448162fcec3c58454e6013e9096684620371a0c
gellwanger <gustavojce@gmail.com>
2022-02-09 23:04:22 -0300

    refatora funçoes e adiciona limite

--- Caixa Eletrônico/conta.py
+++ Caixa Eletrônico/conta.py
@@ -18,7 +18,22 @@ class Conta:
     def saque(self, valor):
         self.__saldo -= valor
 
-    def transfere(self, valor, origem, destino):
-        origem.saque(valor)
+    def transfere(self, valor, destino):
+        self.saque(valor)
         destino.deposito(valor)
-        
+
+    @property
+    def saldo(self):
+        return self.__saldo
+
+    @property
+    def titular(self):
+        return self.__titular
+    
+    @property
+    def limite(self):
+        return self.__limite
+
+    @limite.setter
+    def limite(self, limite):
+        self.__limite = limite
